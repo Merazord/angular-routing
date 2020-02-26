24ce52e43a880a01d042a9ee58b194d12e8b13bf
Merazord <jose.angel.meraz@gmail.com>
2020-02-26 09:22:33 -0600

    Primer Commit

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: { id: number, name: string, status: string };
+  serverName = '';
+  serverStatus = '';
+  allowEdit = false;
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+    }
+    );
+    this.route.fragment.subscribe();
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+  }
+
+}
---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number, name: string };
+  paramsSuscription: Subscription;
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    this.paramsSuscription = this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
+  }
+  ngOnDestroy() {
+    this.paramsSuscription.unsubscribe();
+  }
+
+}
